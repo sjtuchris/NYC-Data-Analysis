572cc4584b628ea91a3351b875786c1d6e90a8bb
Lili <lilixu93@icloud.com>
2017-11-26 17:26:59 -0500

    data clean update
    add groupby case creation data

--- data_clean.py
+++ data_clean.py
@@ -1,16 +1,16 @@
-#Read csv into dataframes
+# read csv into dataframes
 from pyspark.sql import SQLContext
 sqlContext = SQLContext(sc)
-#df = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").load("2009.csv")
+# df = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").load("2009.csv")
 
-df = sqlContext.read.load('2009.csv', 
-                          format='com.databricks.spark.csv', 
-                          header='true', 
+df = sqlContext.read.load('2009.csv',
+                          format='com.databricks.spark.csv',
+                          header='true',
                           inferSchema='true')
 # count number of rows
 df.count()
 
-#describe the rows, see numeric min/max values for any numerical illegal values
+# describe the rows, see numeric min/max values for any numerical illegal values
 df.describe().show()
 
 from pyspark.sql.functions import isnan, when, count, col
@@ -20,4 +20,8 @@ df.select([count(when(col(c).isNull(), c)).alias(c) for c in df.columns]).take(1
 df.select([count(when(col(c) == 'N/A', c)).alias(c) for c in df.columns]).take(1)
 # check unspecified values in each row
 df.select([count(when(col(c)=='Unspecified', c)).alias(c) for c in df.columns]).take(1)
+# Community Board column contains '0 Unspecified'
+df.select([count(when(col(c)=='0 Unspecified', c)).alias(c) for c in df.columns]).take(1)
 
+# Group by creation Date to see abnormal
+df.groupBy('Created Date').count().describe().show()
