3d5a9dee3357a43eaec9f24b9c43fc2c3faf1540
Lili <lilixu93@icloud.com>
2017-11-26 16:39:10 -0500

    data clean check
    check invalid rows for each column.

---
+++ data_clean.py
@@ -0,0 +1,23 @@
+#Read csv into dataframes
+from pyspark.sql import SQLContext
+sqlContext = SQLContext(sc)
+#df = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").load("2009.csv")
+
+df = sqlContext.read.load('2009.csv', 
+                          format='com.databricks.spark.csv', 
+                          header='true', 
+                          inferSchema='true')
+# count number of rows
+df.count()
+
+#describe the rows, see numeric min/max values for any numerical illegal values
+df.describe().show()
+
+from pyspark.sql.functions import isnan, when, count, col
+# check None values in each row
+df.select([count(when(col(c).isNull(), c)).alias(c) for c in df.columns]).take(1)
+# check N/A values in each row
+df.select([count(when(col(c) == 'N/A', c)).alias(c) for c in df.columns]).take(1)
+# check unspecified values in each row
+df.select([count(when(col(c)=='Unspecified', c)).alias(c) for c in df.columns]).take(1)
+
