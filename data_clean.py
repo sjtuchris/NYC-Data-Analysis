163754b0f384db7ecd4335f9db6751aceaffbabe
Lili <lilixu93@icloud.com>
2017-11-28 22:14:28 -0500

    add groupby
    to detect suspicious data

--- data_clean.py
+++ data_clean.py
@@ -1,19 +1,16 @@
-# read csv into dataframes
 from pyspark.sql import SQLContext
-sqlContext = SQLContext(sc)
-# df = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").load("2009.csv")
+from pyspark.sql.functions import isnan, when, count, col, length
 
-df = sqlContext.read.load('2009.csv',
-                          format='com.databricks.spark.csv',
-                          header='true',
-                          inferSchema='true')
+sqlContext = SQLContext(sc)
+
+# read csv into dataframes
+df = sqlContext.read.load('20*.csv', format='com.databricks.spark.csv', header='true', inferSchema='true')
 # count number of rows
 df.count()
 
 # describe the rows, see numeric min/max values for any numerical illegal values
 df.describe().show()
 
-from pyspark.sql.functions import isnan, when, count, col
 # check None values in each row
 df.select([count(when(col(c).isNull(), c)).alias(c) for c in df.columns]).take(1)
 # check N/A values in each row
@@ -25,3 +22,23 @@ df.select([count(when(col(c)=='0 Unspecified', c)).alias(c) for c in df.columns]
 
 # Group by creation Date to see abnormal
 df.groupBy('Created Date').count().describe().show()
+df.groupBy('Closed Date').count().describe().show()
+
+# Group by to detect suspicious data
+df.groupBy('Agency').count().sort('count').show()
+df.groupBy('Agency Name').count().sort('count').show()
+df.groupBy('Complaint Type').count().sort('count').show()
+df.groupBy('Address Type').count().sort('count').show()
+df.groupBy('City').count().sort('count').show()
+df.groupBy('Facility Type').count().sort('count').show()
+df.groupBy('Borough').count().sort('count').show()
+df.groupBy('Park Borough').count().sort('count').show()
+
+# Take a look at the suprisingly high occurance
+df.groupBy('Closed Date').count().orderBy('count',ascending=False).take(1)
+
+df.select(col('Incident Zip')).filter(col('Incident Zip').rlike('^(\d{5}(-)?(\d{4})?|[A-Z]\d[A-Z] ?\d[A-Z]\d)$')== False).show()
+
+# Check abnormal Zip Code
+df.where(length(col('Incident Zip')) > 0).select(col('Incident Zip')).filter(col('Incident Zip').rlike('^(\d{5}(-)?(\d{4})?|[A-Z]\d[A-Z] ?\d[A-Z]\d)$')==False).collect()
+df.where(length(col('Incident Zip')) > 0).select(col('Incident Zip')).filter(col('Incident Zip').rlike('^(\d{5}(-)?(\d{4})?|[A-Z]\d[A-Z] ?\d[A-Z]\d)$')==False).groupBy('Incident Zip').count().show()
