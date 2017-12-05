8e2d2032844d7a46ca91db4c5326ef332525cd5d
Lili <lilixu93@icloud.com>
2017-12-05 18:14:01 -0500

    further_exploration_update
    Added income, education

--- further_exploration.py
+++ further_exploration.py
@@ -1,6 +1,3 @@
-df_s = sqlContext.read.load('population.csv', format='com.databricks.spark.csv', header='true', inferSchema='true')
-df_c = sqlContext.read.load('population_count.csv', format='com.databricks.spark.csv', header='true', inferSchema='true')
-
 # Read Weather Dataset
 df_w = sqlContext.read.load('weather.csv', format='com.databricks.spark.csv', header='true', inferSchema='true')
 
@@ -21,3 +18,26 @@ count_temp_joined = df_2016_count.join(df_w, df_2016_count['day'] == df_w['day']
 # Filter heating complaints
 df_2016_h_count = df_2016.filter(col('Complaint Type')=='HEAT/HOT WATER').groupBy('day').count()
 h_count_temp_joined = df_2016_h_count.join(df_w, df_2016_count['day'] == df_w['day']).sort('count',ascending=False)
+
+# Read
+df_edu = sqlContext.read.load('degree_original.csv', format='com.databricks.spark.csv', header='true', inferSchema='true')
+df_income = sqlContext.read.load('income_original.csv', format='com.databricks.spark.csv', header='true', inferSchema='true')
+
+df_count_zip = df.groupBy('Incident Zip').count()
+
+# Join education level
+zip_edu_joined = df_count_zip.join(df_edu, df_count_zip['Incident Zip'] == df_edu['zip'])
+
+# Join income level
+zip_income_joined = df_count_zip.join(df_income, df_count_zip['Incident Zip'] == df_income['zip'])
+
+# Join all
+all_joined = zip_edu_joined.join(df_income,  zip_edu_joined['Incident Zip'] == df_income['zip'])
+
+# Get education index
+all_joined = all_joined.withColumn('edu_index', all_joined['degree']/all_joined['household'])
+
+# Save to csv for visualtion
+zip_edu_joined.save('zip_edu','com.databricks.spark.csv')
+zip_income_joined.save('zip_income','com.databricks.spark.csv')
+all_joined.save('zip_edu2','com.databricks.spark.csv')
