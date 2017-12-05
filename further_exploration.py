be024c99a4aaef252888108f881e58647a662c22
Lili <lilixu93@icloud.com>
2017-12-05 16:58:48 -0500

    future investigation

---
+++ further_exploration.py
@@ -0,0 +1,23 @@
+df_s = sqlContext.read.load('population.csv', format='com.databricks.spark.csv', header='true', inferSchema='true')
+df_c = sqlContext.read.load('population_count.csv', format='com.databricks.spark.csv', header='true', inferSchema='true')
+
+# Read Weather Dataset
+df_w = sqlContext.read.load('weather.csv', format='com.databricks.spark.csv', header='true', inferSchema='true')
+
+from datetime import datetime
+from pyspark.sql.functions import col, udf
+
+# Transform time format to string corresponding to existing dataset
+func =  udf (lambda x: datetime.strptime(x, '%d-%m-%Y').strftime('%m/%d/%Y'))
+df_w = df_w.withColumn('day', func(col('date')))
+
+# GroupBy Day
+df_2016_count = df_2016.groupBy('day').count()
+
+# Join the weather table
+df_2016_count.join(df_w, df_2017_count['day'] == df_w['day']).sort('count',ascending=False).show()
+count_temp_joined = df_2016_count.join(df_w, df_2016_count['day'] == df_w['day']).sort('count',ascending=False)
+
+# Filter heating complaints
+df_2016_h_count = df_2016.filter(col('Complaint Type')=='HEAT/HOT WATER').groupBy('day').count()
+h_count_temp_joined = df_2016_h_count.join(df_w, df_2016_count['day'] == df_w['day']).sort('count',ascending=False)
